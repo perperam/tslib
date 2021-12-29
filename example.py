95b1799a45be8d509ca050d286902cf8d78985f5
robin <83653446+perperam@users.noreply.github.com>
2021-12-29 19:51:07 +0100

    Finished basic functionality

--- example.py
+++ example.py
@@ -1,15 +1,9 @@
 from tslib import ts_query
 from secrets import *
 
-
 with ts_query(HOST) as ts:
-    ts.write(f"login serveradmin {PW}\n".encode())
-    print(ts.read_until("...".encode(), timeout=2))
-    ts.write("use 1\n".encode())
-    print(ts.read_until("...".encode(), timeout=2))
-    ts.write("serveredit virtualserver_name=TEST\n".encode())
-    print(ts.read_until("...".encode(), timeout=2))
-
-    # print(ts.login_as_admin(PW))
-    # print(ts.write("virtualserver_name=TEST\n".encode()))
-    # print(ts.ts_command("virtualserver_name=TEST"))
+    print(ts.login_as_admin(PW))
+    print(ts.edit_virtualserver_name("TEST11"))
+    print("OK")
+    _, reply = ts.execute("help")
+    print(reply.decode())
--- tslib.py
+++ tslib.py
@@ -2,7 +2,6 @@
 AUTHOR: https://github.com/perperam
 EMAIL: perperam@icatas.eu
 """
-
 from telnetlib import Telnet
 
 class ts_query(Telnet):
@@ -18,37 +17,37 @@ class ts_query(Telnet):
     def __init__(self, host, port=TELNET_PORT):
         Telnet.__init__(self, host, port)
 
-    def ts_command(self, command):
+    def execute(self, command):
         self.write(f"{command}\n".encode())
         return self.feedback()
 
     def feedback(self):
         # test if the server has send no error back
         reply = self.read_until(self.NO_ERROR.encode(), timeout=self.TIMEOUT)
-        if reply == self.NO_ERROR.encode():
-            return True, None
-        else:
-            return False, reply
-
+        if reply[-17:] == self.NO_ERROR.encode():
+            return True, reply
+        # print(reply[-17:], self.NO_ERROR.encode())
+        return False, reply
 
 
     def login(self, user, password):
         # Testing if the login is correct by catching the welcome message
         reply = self.read_until(self.WELCOME_MSG.encode(), timeout=self.TIMEOUT)
         if reply == self.WELCOME_MSG.encode():
-            self.write(f"login {user} {password}\n".encode())
-            # testing if "error id=0 msg=ok" is returned by the server
-            done, reply = self.feedback()
+            done, reply = self.execute(f"login {user} {password}")
             if done:
-                return True, None
+                return True, reply
         return False, reply
 
     def login_as_admin(self, password, virtual_server=1):
         user = "serveradmin"
         done, reply = self.login(user, password)
-        if not done: return False, reply
-        print("try to use 1")
-        self.write(f"use {virtual_server}\n".encode())
-        done, reply = self.feedback()
-        if not done: return False, reply
-        return True, None
+        if not done:
+            return False, reply
+        done, reply = self.execute(f"use {virtual_server}")
+        if not done:
+            return False, reply
+        return True, reply
+
+    def edit_virtualserver_name(self, name):
+        return self.execute(f"serveredit virtualserver_name={name}")
